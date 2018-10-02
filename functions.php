cff90ac11ad8c3848fdde40b38d729c1d10c6647
Jieck Loo <377592235@qq.com>
2018-10-03 02:12:59 +0800

    beta 1.0

---
+++ functions.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+function themeConfig($form) {
+    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
+    $form->addInput($logoUrl);
+    
+    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
+    array('ShowRecentPosts' => _t('显示最新文章'),
+    'ShowRecentComments' => _t('显示最近回复'),
+    'ShowCategory' => _t('显示分类'),
+    'ShowArchive' => _t('显示归档'),
+    'ShowOther' => _t('显示其它杂项')),
+    array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
+    
+    $form->addInput($sidebarBlock->multiMode());
+}
+
+
+/*
+function themeFields($layout) {
+    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点LOGO地址'), _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO'));
+    $layout->addItem($logoUrl);
+}
+*/
+
+//判断是否存在图片
+function is_img($thiz){
+    $content = $thiz->content;
+    $ret = preg_match("/\<img.*?src\=\"(.*?)\"[^>]*>/i", $content, $thumbUrl);
+    if($ret === 1 && count($thumbUrl) == 2){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+//获取文章中第一张图片
+function img_postthemb($thiz){
+    $content = $thiz->content;
+    $ret = preg_match("/\<img.*?src\=\"(.*?)\"[^>]*>/i", $content, $thumbUrl);
+    if($ret === 1 && count($thumbUrl) == 2){
+        return "<div class=post-themb style=background-image:url(" . $thumbUrl[1] . ")><a href=" . $thiz->permalink . "></a></div>";
+    }else{
+        return "";
+    }
+}
+
+//获取文章中的视频
+function video_postthemb($thiz){
+    $content = $thiz->content;
+    $ret = preg_match("/\<iframe.*>.*\<\/iframe>/i", $content, $thumbUrl);
+    if($ret === 1 && count($thumbUrl) == 1){
+        return "<div class=post-themb_video>" . $thumbUrl[0] . "</div>";
+    }else{
+        return "";
+    }
+}
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 商务版在原来的基础上，增加了首页缩略图预览，基于Typecho 1.1
+ * 
+ * @package 16th-商务版
+ * @author Jieck Loo
+ * @version beta 1.0
+ * @link https://jieck.cn
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+ $this->need('header.php');
+ ?>
+
+<div class="col-mb-12 col-8" id="main" role="main">
+	<?php while($this->next()): ?>
+        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+			<ul class="post-meta">
+				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
+				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+			</ul>
+			<?php 
+			//判断是否存在图片
+			$is_img = is_img($this);
+			if($is_img==1){
+				echo img_postthemb($this);
+			}else{
+				echo video_postthemb($this);
+			}
+			?>
+            <div class="post-content" itemprop="articleBody">
+    			<?php if($is_img==1){$this->excerpt(100,'...');}else{$this->excerpt(200,'...');} ?>
+            </div>
+        </article>
+	<?php endwhile; ?>
+
+    <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
+</div><!-- end #main-->
+
+<?php $this->need('sidebar.php'); ?>
+<?php $this->need('footer.php'); ?>
